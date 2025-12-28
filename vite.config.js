03e871b500c79bce85e398a8815843f6da9d5f89
vijayelectronic <vijayelectronic689@gmail.com>
2025-12-28 11:53:16 +0530

    commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Relative base path for GitHub Pages compatibility
+  base: '/Electronics-Service-near-Unnao/', // Changed base path for specific GitHub repo
 })
